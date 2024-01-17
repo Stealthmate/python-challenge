0bcb4bae69d33efa6d3deeffbbaebbb43c7d0ecb
stealthmate <stealthmate1233@gmail.com>
2024-01-17 17:56:31 +0900

    feat: level 3

---
+++ level3.py
@@ -0,0 +1,12 @@
+import re
+
+PATTERN = re.compile(r"[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]")
+
+text = ""
+with open("level3.txt") as f:
+    text = f.read()
+
+results = re.findall(PATTERN, text)
+target = "".join(results)
+
+print(f"http://www.pythonchallenge.com/pc/def/{target}.html")
